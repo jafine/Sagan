fe6ab7b53fdddd920a4119988ed070b190c6171f
AlexCollins <Alexander@MacBook-Pro.local>
2016-10-05 13:49:11 +1100

    upgrae tag to 1.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'Sagan',
   packages = ['Sagan'],
-  version = '1.2',
+  version = '1.4',
   description = 'Sagan library for cuberider.',
   author = 'Alexander William Collins',
   author_email = 'alexcollins010@gmail.com',
   url = 'https://github.com/AlexanderCollins/Sagan',
-  download_url = 'https://github.com/AlexanderCollins/Sagan/tarball/1.2',
+  download_url = 'https://github.com/AlexanderCollins/Sagan/tarball/1.4',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
