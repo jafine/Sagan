cea44867c22ba14ed93a46b4716c5fc5dff9426e
AlexCollins <Alexander@MacBook-Pro.local>
2016-10-05 14:39:54 +1100

    1.8

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'Sagan',
   packages = ['Sagan'],
   install_requires=[]
-  version = '1.7',
+  version = '1.8',
   description = 'Sagan library for cuberider.',
   author = 'Alexander William Collins',
   author_email = 'alexcollins010@gmail.com',
   url = 'https://github.com/AlexanderCollins/Sagan',
-  download_url = 'https://github.com/AlexanderCollins/Sagan/tarball/1.7',
+  download_url = 'https://github.com/AlexanderCollins/Sagan/tarball/1.8',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
